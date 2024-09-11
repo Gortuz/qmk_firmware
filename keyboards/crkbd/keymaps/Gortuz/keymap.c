aad09eca77cbce345b6e70445de54edf11da6738
Alan Sandoval <alansandovalproject@gmail.com>
2024-09-11 18:54:32 -0500

    Tab and ESC

--- keyboards/crkbd/keymaps/Gortuz/keymap.c
+++ keyboards/crkbd/keymaps/Gortuz/keymap.c
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT_split_3x6_3( // Base
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-LT(4, KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+LT(4, KC_ESC),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+LCTL_T(KC_TAB),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_MINS),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                  KC_LALT,   LT(1, KC_LGUI),  KC_SPC,     LGUI_T(KC_SPC),   LT(2, KC_ENT), KC_RALT
+                                  LALT_T(KC_LGUI),   MO(1),  KC_SPC,     LGUI_T(KC_SPC),   LT(2, KC_ENT), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
     [1] = LAYOUT_split_3x6_3( // Arrows
